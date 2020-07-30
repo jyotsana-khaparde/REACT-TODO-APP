c2e78909f2ba19ab0ae5ce77c8dbbcaf592431c1
Jyotsana Khaparde <jyotsanakhaparde0597@gmail.com>
2020-07-30 09:08:53 +0530

    practice of inline style and using js inside jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'; 
+/**
+ * In jsx we use className instead of class reason :-
+ *  1. people say class is a reserverd key word in javascript
+ *  2. jsx just using the vanilla javascript DOM API
+ *  3. document.getElementById('sometging').className + "new-class-name"
+ *  4. className is a underline javascript DOM API
+ */
 function App() {
+ const date = new Date()
+ const hours = date.getHours()
+ let timeOfDay
+ const styles = {
+   color: 'black'
+ }
+
+ if(hours < 12) {
+   timeOfDay = 'Morning'
+   styles.color = 'green'
+ } else if(hours >= 12 && hours < 17){
+   timeOfDay = 'Afternoon'
+   styles.color = 'blue'
+ } else {
+   timeOfDay = 'Night'
+   styles.color = 'orange'
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 style={styles}>Hello, Good {timeOfDay} jyotsana This is your TODO list</h3>
+
+      <input type='checkbox'></input>
+      <p>Placeholder text here</p>
+
+      <input type='checkbox'></input>
+      <p>Placeholder text here</p>
+
+      <input type='checkbox'></input>
+      <p>Placeholder text here</p>
+
+      <input type='checkbox'></input>
+      <p>Placeholder text here</p>
     </div>
   );
 }

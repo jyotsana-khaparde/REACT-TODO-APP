0dc4fc36012b9a682eaeeadf72fa9ef014e814cc
Jyotsana Khaparde <jyotsanakhaparde0597@gmail.com>
2020-08-05 08:27:02 +0530

    added css when task is completed and for loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import TodoItem from './component/todoItem';
 import todoData from './jsonFiles/todoData'
+import { render } from '@testing-library/react';
 /**
  * In jsx we use className instead of class reason :-
  *  1. people say class is a reserverd key word in javascript
@@ -9,33 +10,65 @@ import todoData from './jsonFiles/todoData'
  *  3. document.getElementById('sometging').className + "new-class-name"
  *  4. className is a underline javascript DOM API
  */
-function App() {
- const date = new Date()
- const hours = date.getHours()
- let timeOfDay
- const styles = {
-   color: 'black'
- }
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todoData,
+      isLoading: true
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
- if (hours < 12) {
-   timeOfDay = 'Morning'
-   styles.color = 'green'
- } else if (hours >= 12 && hours < 17) {
-   timeOfDay = 'Afternoon'
-   styles.color = 'blue'
- } else {
-   timeOfDay = 'Night'
-   styles.color = 'orange'
- }
+  handleChange = (id) => {
+    console.log("changed", id);
+    const updatedState = this.state.todos.map(item => {
+      if (item.id === id) {
+          item.completed = !item.completed
+        }
+        return item;
+    })
+    this.setState({
+        todos: updatedState
+    })
+    console.log('updatedState----', updatedState);
+  }
 
- const todoItemList = todoData.map(item => <TodoItem key={item.id} todoItem={item}/>)
+  componentDidMount() {
+    setTimeout(() =>{
+      this.setState({
+        isLoading: false
+      })
+    }, 1500)
+  }
 
-  return (
+  render() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    const styles = {
+      color: 'black'
+    }
+
+    if (hours < 12) {
+      timeOfDay = 'Morning'
+      styles.color = 'green'
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = 'Afternoon'
+      styles.color = 'blue'
+    } else {
+      timeOfDay = 'Night'
+      styles.color = 'orange'
+    }
+
+    const todoItemList = this.state.todos.map(item => <TodoItem key={item.id} todoItem={item} handleChange={this.handleChange}/>);
+    return (
     <div className="todo-list">
       <h3 style={styles}>Hello, Good {timeOfDay} jyotsana This is your TODO list</h3>
-      {todoItemList}
+      {this.state.isLoading ? 'Loading....' : todoItemList }
     </div>
   );
+  };
 }
 
 export default App;
--- src/component/todoItem.js
+++ src/component/todoItem.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
 function TodoItem(props) {
-    console.log(props);
-    return(
+    console.log('props-TodoItem-->', props);
+    const completedStyles = {
+        color: '#cdcdcd',
+        textDecoration: 'line-through',
+        fontStyle: 'italic'
+    }
+        return (
         <div className='todo-items'>
-            <input type='checkbox' defaultChecked={props.todoItem.completed} ></input>
-            <p style={{color: props.todoItem.text ? 'black' : 'gray' }}>{props.todoItem.text || 'None'}</p>
+            <input
+                type='checkbox'
+                checked={props.todoItem.completed} 
+                onChange={() => props.handleChange(props.todoItem.id)}></input>
+                {
+                    props.todoItem.completed ?
+                    <p style={completedStyles}> {props.todoItem.text} </p>
+                    : <p style={{color: props.todoItem.text ? 'black' : 'gray' }}>
+                        {props.todoItem.text || 'None'}
+                    </p>
+                }
         </div>
-    )
-}
+        )
+ }
 
 export default TodoItem;
